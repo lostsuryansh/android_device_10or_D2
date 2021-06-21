ec68dbd3cd32f16c04d83a02f91c43ce656b8ea5
lostsuryansh <lostsuryansh@gmail.com>
2021-06-21 18:32:01 +0530

    gdhd

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,9 +9,12 @@
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# WITHOUT WAlineageANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_D2.mk
+
+COMMON_LUNCH_CHOICES := \
+     lineage_D2-userdebug
